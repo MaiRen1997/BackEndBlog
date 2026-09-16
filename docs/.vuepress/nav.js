a4d24395b00ef95ff62b4a96460b75fb3be69d12
MaiRen <2231101379@qq.com>
2026-09-16 15:58:54 +0800

    修改redis的使用路径

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,25 +1,5 @@
 module.exports = [
   { text: '首页', link: '/' },
-  // {
-  //   text: '前端',
-  //   link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-  //   items: [
-  //     // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-  //     {
-  //       text: 'javaScript笔记精选',
-  //       items: [
-  //         { text: 'JS常用案例', link: '/pages/03dd97/' },
-  //       ],
-  //     },
-  //     {
-  //       text: 'vue学习精选',
-  //       items: [
-  //         { text: '《vue基本知识点》', link: '/pages/0082e9/' },
-  //         { text: '《vue3的使用》', link: '/pages/24e9bb/' }
-  //       ],
-  //     },
-  //   ],
-  // },
   {
     text: 'Python基础',
     link: '/python/'
@@ -48,6 +28,10 @@ module.exports = [
         ],
       }
     ]
+  },
+  {
+    text: 'Redis的使用',
+    link: '/Redis/',
   },
   {
     text: 'Mysql',
